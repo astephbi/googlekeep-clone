9b0b8b9dae64b279282dee527efd506919bda761
Alex <astephbi@gmail.com>
2022-06-07 23:48:08 +0200

    première partie sans logique

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Le clone de Google Keep</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>Titre</h1>
+      <p>Contenu</p>
+    </div>
+  );
+};
+
+export default Note;
